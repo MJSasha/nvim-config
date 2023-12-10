1c5a9a7c13c02a7e2c6c4a5310d50ea9731b6619
MJSasha <sasha.ushankov@gmail.com>
2023-12-11 01:23:38 +0300

    Fixed bindings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -25,7 +25,6 @@ vim.keymap.set('n', '<leader>w', ':w<CR>')
 vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
 vim.keymap.set('n', '<leader>X', ':BufferLineCloseRight<CR>')
 vim.keymap.set('n', '<leader>s', ':BufferLineSortByTabs<CR>')
-vim.keymap.set('i', 'jj', '<Esc>')
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
 -- Tabs
