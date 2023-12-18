c2fe7a777ddd0c76e9774ac31dcac5e16191f5ea
MJSasha <sasha.ushankov@gmail.com>
2023-12-18 12:52:25 +0300

    Added current file opening in system explorer

--- lua/custom-plugins/system-explorer-opener.lua
+++ lua/custom-plugins/system-explorer-opener.lua
@@ -1,24 +1,35 @@
 local M = {}
 
 function M.open_directory_in_explorer()
-    local current_directory = vim.fn.getcwd()
-    local command = ""
+    local current_buffer = vim.api.nvim_get_current_buf()
+    local current_file = vim.api.nvim_buf_get_name(current_buffer)
+    local current_directory = vim.fn.fnamemodify(current_file, ":h")
+    open(current_directory)
+end
 
-    if vim.fn.has("win32") == 1 then
-        command = "start " .. current_directory
-    elseif vim.fn.has("mac") == 1 then
-        command = "open " .. current_directory
-    elseif vim.fn.has("unix") == 1 then
-        command = "xdg-open " .. current_directory
-    end
-
-    vim.fn.system(command)
+function M.open_root_directory_in_explorer()
+    local root_directory = vim.fn.getcwd()
+    open(root_directory)
 end
 
 function M.setup(opts)
     opts = opts or {}
-    vim.cmd(
-    [[command! OpenInSystemExplorer lua require'custom-plugins.system-explorer-opener'.open_directory_in_explorer()]])
+    vim.cmd([[command! OpenInSystemExplorer lua require'custom-plugins.system-explorer-opener'.open_directory_in_explorer()]])
+    vim.cmd([[command! OpenRootInSystemExplorer lua require'custom-plugins.system-explorer-opener'.open_root_directory_in_explorer()]])
+end
+
+function open(directory)
+    local command = ""
+
+    if vim.fn.has("win32") == 1 then
+        command = "start " .. directory
+    elseif vim.fn.has("mac") == 1 then
+        command = "open " .. directory
+    elseif vim.fn.has("unix") == 1 then
+        command = "xdg-open " .. directory
+    end
+
+    vim.fn.system(command)
 end
 
 return M
